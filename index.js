bd58c646d51d823f325639a59410bc2fed7f2fcc
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-09 17:13:46 -0600

    creacion de peticion post

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const data = require("./MOCK_DATA.json");
 const app = express();
+app.use(express.json());
+const Services = require("./src/services");
 require("dotenv").config();
 
 app.get("/", (req, res) => {
-  res.json(data);
+  res.json({
+    message: "users",
+    users: Services.getUsers(),
+  });
+});
+
+app.post("/", (req, res) => {
+  let {body: newUser} = req;
+  res.status(201).json({
+    message: "Usuario creado",
+    body: Services.createUser(newUser),
+  });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`servidor iniciado en localhost:3000`);
+  console.log(`servidor iniciado en http://localhost:3000`);
 });
---
+++ src/services.js
@@ -0,0 +1,13 @@
+const data = require("./MOCK_DATA.json");
+
+module.exports = {
+  getUsers: () => data,
+  createUser: (dataUser) => {
+    let newUser = {
+      id: data.length + 1,
+      ...dataUser,
+    };
+    data.push(newUser);
+    return newUser;
+  },
+};
